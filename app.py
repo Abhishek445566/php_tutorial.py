aa9d4c1d82a2b36a75b4cef59b4b10a4a7b54c34
abhishek-goyal <45648611+Abhishek445566@users.noreply.github.com>
2020-07-03 14:32:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app=Flask(__name__)
 @app.route("/")
   def index():
     names=["Alice","bob","charlie"]
-    return render_template("index.html",names=names)
+    return render_template("indexy.html",names=names)
