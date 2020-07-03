248422689272d467a6f717715b533e1838e22ac3
abhishek-goyal <45648611+Abhishek445566@users.noreply.github.com>
2020-07-03 14:24:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask,render_template,request
+from flask import Flask
 
 app=Flask(__name__)
 
 @app.route("/")
   def tutorial():
-    return render_template("tutorial.html")
+    return f"<h1>hello</h1>"
